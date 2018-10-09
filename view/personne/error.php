4d0fb00ca98ba3d880be0142fd10a2e38676d46c
David <43638224+Davidsambuc@users.noreply.github.com>
2018-10-09 18:23:32 +0200

    view pers

---
+++ view/personne/error.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Error</title>
+    </head>
+    <body>
+        <?php
+     echo "404: Personne Not Found";
+        ?>
+    </body>
+</html>
+
