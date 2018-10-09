9e35193c2b7c8c0f15c13ac7e8d11549b86335ce
Davidsambuc <lol8544@gmail.com>
2018-10-09 13:20:14 +0200

    Revert "initialisation"
    This reverts commit d81bb41a2bc107778b3ef5672cb1ac963da61a92.

--- Mon AMAP/Mon AMAP/controller/routeur.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'ControllerPersonne.php';
-// On recupère l'action passée dans l'URL
-$actionPersonne = $_GET['action'];
-// Appel de la méthode statique $action de ControllerPersonne
-ControllerPersonne::$actionPersonne(); 
-?>
-
