06ef0b7522de0b648b54f0ec769054eb16cd4674
Louise R <louiserobert2510@gmail.com>
2018-10-19 18:23:44 +0200

    implémentation du menu (reste à perfectionner -> sous menus)

---
+++ view/menu.php
@@ -0,0 +1,23 @@
+
+<!-- <body> -->
+<ul id="menu">
+
+<li class="nousConnaitre"><a href="File::build_path(array("view","nousConnaitre","connaitre.php"))">Nous connaitre</a></li>
+	<ul>
+      <li><a href="/home/ann2/robertl/Documents/AMAP/monAMAP/view/NousConnaitre/lamap.html">l'AMAP</a></li>
+      <li><a href="/home/ann2/robertl/Documents/AMAP/monAMAP/view/NousConnaitre/nousContacter.html">Nous contacter</a></li>
+      <li><a href="/home/ann2/robertl/Documents/AMAP/monAMAP/view/NousConnaitre/nousTrouver.html">Nous trouver</a></li>
+    </ul>
+<li class="nosProduits"><a href="./NosProduits/produits.html">Nos produits</a></li>
+
+<li class="nosContrats"><a href="./NosContrats/contrats.html">Nos contrats</a></li>
+
+<li class="laVieAlAMAP"><a href="./LaVieALAMAP/LVALA.html">La vie à l'AMAP</a></li>
+
+<li class="monProfil"><a href="./MonProfil/profil.html">Mon profil</a></li>
+
+<li class="nousSoutenir"><a href="./NousSoutenir/soutient.html">Nous soutenir</a></li>
+
+</ul>
+
+<!-- </body>
