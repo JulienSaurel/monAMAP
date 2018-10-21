a0bc2d65cd70200e9b5966e54932ab5b479ede31
LouiseRobert <43638358+LouiseRobert@users.noreply.github.com>
2018-10-21 14:26:56 +0200

    Update view/menu.php

--- view/menu.php
+++ view/menu.php
@@ -11,15 +11,30 @@
       <li><a href="?action=display3rd&controller=nousConnaitre">Nous contacter</a></li>
       <li><a href="?action=display4th&controller=nousConnaitre">Nous trouver</a></li>
     </ul>
-<li class="nosProduits"><a href="?action=display&controller=nosProduits">Nos produits</a></li>
-
-<li class="nosContrats"><a href="?action=display&controller=nosContrats">Nos contrats</a></li>
-
-<li class="laVieAlAMAP"><a href="?action=display&controller=laVieAlAMAP">La vie à l'AMAP</a></li>
-
-<li class="monProfil"><a href="?action=display&controller=monProfil">Mon profil</a></li>
-
-<li class="nousSoutenir"><a href="?action=display&controller=nousSoutenir">Nous soutenir</a></li>
+<li class="nosProduits"><a href="?action=display5th&controller=nosProduits">Nos produits</a></li>
+	<ul>
+      <li><a href="?action=display6th&controller=nosProduits">Nos producteurs</a></li>
+      <li><a href="?action=display7th&controller=nosProduits">Produits du moment</a></li>
+    </ul>
+<li class="nosContrats"><a href="?action=display8th&controller=nosContrats">Nos contrats</a></li>
+	<ul>
+      <li><a href="?action=display9th&controller=nosContrats">Laitier</a></li>
+      <li><a href="?action=display10th&controller=nosContrats">Viande</a></li>
+      <li><a href="?action=display11th&controller=nosContrats">Légumes</a></li>
+	  <li><a href="?action=display12th&controller=nosContrats">Mix</a></li>
+    </ul>
+<li class="laVieAlAMAP"><a href="?action=display13th&controller=laVieAlAMAP">La vie à l'AMAP</a></li>
+	<ul>
+      <li><a href="?action=display14th&controller=laVieAlAMAP">Articles</a></li>
+      <li><a href="?action=display15th&controller=laVieAlAMAP">Evénements</a></li>
+	  <li><a href="?action=display16th&controller=laVieAlAMAP">Livre d'or</a></li>
+    </ul>
+<li class="monProfil"><a href="?action=display17th&controller=monProfil">Mon profil</a></li>
+	<ul>
+      <li><a href="?action=display18th&controller=monProfil">Voir mon profil</a></li>
+      <li><a href="?action=display19th&controller=monProfil">Devenir producteur</a></li>
+    </ul>
+<li class="nousSoutenir"><a href="?action=display20th&controller=nousSoutenir">Nous soutenir</a></li>
 
 </ul>
 </nav>
