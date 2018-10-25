1265ccc90ac986d5d2091eaad4900c8efe33138d
Louise R <louiserobert2510@gmail.com>
2018-10-25 17:25:55 +0200

    début de votre profil

--- config/Conf.php
+++ config/Conf.php
@@ -6,16 +6,16 @@ class Conf {
     static private $databases = array(
         // Le nom d'hote est webinfo a l'IUT
         // ou localhost sur votre machine
-        'hostname' => 'localhost',
+        'hostname' => 'webinfo',
         // A l'IUT -> login
         // Chez vous -> vous le coisissez en creant la base
-        'database' => 'Mon AMAP',
+        'database' => 'robertl',
         // A l'IUT, c'est votre login
         // Sur votre machine, vous avez surement un compte 'root'
-        'login' => 'root',
+        'login' => 'robertl',
         // A l'IUT, c'est votre mdp (INE par defaut)
         // Pas de mdp en local par defaut
-        'password' => ''
+        'password' => 'chapeaumelon'
     );
 
     static public function getDebug() {
--- view/monProfil/voirmonprofil.php
+++ view/monProfil/voirmonprofil.php
@@ -1,1 +1,19 @@
-Voir profil
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Mon Profil</title>
+  <!-- <link rel="stylesheet" href="style.css"> -->
+</head>
+<body>
+	<h1> Votre profil: </h1>
+	<nav>
+		<ul>
+			<li>Pseudo: </li>
+			<li>Nom: </li>
+			<li>Prénom: </li>
+			<li>e-mail: </li>
+		</ul>
+	</nav>
+</body>
+</html>
