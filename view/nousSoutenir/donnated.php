f4a5c46f3d975b59758b6063b35fe9bd96d896bf
Louise R <louiserobert2510@gmail.com>
2018-11-13 18:20:01 +0100

    intégration du fichier pdf téléchargeable (non dynamique) a la page de dons et intégration de la carte google maps à nousTrouver <3

--- view/nousSoutenir/donnated.php
+++ view/nousSoutenir/donnated.php
@@ -1,2 +1,5 @@
 <H2> Merci <?php echo $donnateur->get('prenomDonnateur') ?>! </H2>
 <p> Au total, vous avez aidé l'AMAP de <?php echo $donnateur->get('montantTotal') ?> € ! </p>
+
+
+<a href="files/facture.pdf" target="_blank"> télécharger</a>
