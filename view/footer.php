1f7eb1df77cce65794e4f28d65ce79455619c97f
Julien Saurel <julien.saurel11@gmail.com>
2018-11-14 16:10:54 +0100

    footer

--- view/footer.php
+++ view/footer.php
@@ -1,5 +1,5 @@
     <div id="logos">
-    	<p> Suivez nous sur les reseaux sociaux ! <p>
+    	<p> Suivez nous sur les réseaux sociaux ! <p>
         <a  href="https://www.facebook.com/"><img id="logoFB" src="images/facebook.png" alt="logo facebook" /> </a>
         <a  href="https://twitter.com/"><img id="logoTT" src="images/twitter.png" alt="logo twitter" /> </a>
         <a  href="https://plus.google.com/"><img id="logoGP" src="images/googleplus.png" alt="logo google plus" /> </a>
