75ce312ab3ab7fdb394c40ae236034274d1f9534
Louise R <louiserobert2510@gmail.com>
2018-11-14 11:08:04 +0100

    calendrier d'événements q

--- view/laVieAlAMAP/evenements.php
+++ view/laVieAlAMAP/evenements.php
@@ -1,1 +1,3 @@
-<p> event </p>
+<h1> Evénements </h1>
+
+<iframe src="https://calendar.google.com/calendar/embed?src=bubfclukh0bhce93aj9lbl1mco%40group.calendar.google.com&ctz=Europe%2FParis" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
