827139d5d41f6bed2a28cc6351ccba88ebe0beec
David <43638224+Davidsambuc@users.noreply.github.com>
2018-11-15 18:30:27 +0100

    ignore conf

--- config/Conf.php
+++ config/Conf.php
@@ -6,17 +6,17 @@ class Conf {
     static private $databases = array(
         // Le nom d'hote est webinfo a l'IUT
         // ou localhost sur votre machine
-        'hostname' => 'webinfo',
+        'hostname' => 'localhost',
         // A l'IUT -> login
         // Chez vous -> vous le coisissez en creant la base
 
-        'database' => 'sambucd',
+        'database' => 'mon amap',
         // A l'IUT, c'est votre login
         // Sur votre machine, vous avez surement un compte 'root'
-        'login' => 'sambucd',
+        'login' => 'root',
         // A l'IUT, c'est votre mdp (INE par defaut)
         // Pas de mdp en local par defaut
-        'password' => '1110011857C'
+        'password' => ''
 
     );
 
