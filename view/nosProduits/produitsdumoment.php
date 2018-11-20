6e5c986d301d97b792f82dff3931e51d2848e16f
Senhaji Salma <salma.senhaji@etu.umontpellier.fr>
2018-11-20 13:46:14 +0100

    amap

--- view/nosProduits/produitsdumoment.php
+++ view/nosProduits/produitsdumoment.php
@@ -1,2 +1,12 @@
+<html>
+
 <h1> Produits du moment: </h1>
 
+<div class="legumes">
+         <img src="images/carottes.jpg" alt="carottes" />
+         <img src="images/courgettes.jpg" alt="courgettes" />
+         <img src="images/artichaud.jpeg" alt="artichaud" />
+          
+</div>
+
+</html>
