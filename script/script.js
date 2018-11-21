927d0f5e9e8331fdd52e8169fd8885cd3c699bd4
Julien Saurel <julien.saurel11@gmail.com>
2018-11-21 12:38:09 +0100

    css menu burger

--- script/script.js
+++ script/script.js
@@ -1,9 +1,17 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.width = "350px";
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 } 
+
+function openLink(){
+	document.getElementsByClassName("linkmenu").children.style.display = "block";
+}
+
+function closeLink(){
+	document.getElementByClassName("linkmenu").children.style.display = "none";
+}
