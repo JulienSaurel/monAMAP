6de9f1503f4714344ea667d8397f9609b1ac759a
Louise R <louiserobert2510@gmail.com>
2018-11-22 15:35:32 +0100

    correction minime sur une view

--- view/nosProduits/produits.php
+++ view/nosProduits/produits.php
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<p> Produits </p>
-=======
-=======
->>>>>>> parent of 6343953... correction
-<html>
-<head>
-<link rel="stylesheet" href="styles_produits.css" />
 
-</head>
-<body>
 <h1> Produits </h1>
 
-<p>Nous proposons une varité de produits: des légumes et fruits de saison. Nous visons à satisfaire nos consommateurs par la qualité de nos produits.s</p>
-
-<div class="diapo">
-
-</div> 
-
-
-
-
-</body>
-
-<<<<<<< HEAD
-</html>
->>>>>>> parent of 6343953... correction
-=======
-</html>
->>>>>>> parent of 6343953... correction
