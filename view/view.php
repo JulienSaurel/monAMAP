b6e4307558f3fdcbbf2b8d38db5803109ad66787
Julien Saurel <julien.saurel11@gmail.com>
2018-11-22 18:58:36 +0100

    css

--- view/view.php
+++ view/view.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
-    <head>
+    <head>  
+        <link rel="icon" type="image/png" href="images/nature.png" />
        	<link rel="stylesheet" type="text/css" href="css/styles.css">
         <script src="script/jquery.min.js"></script>
         <script src="script/script.js"></script>
@@ -11,8 +12,10 @@
     
     	<body>
         <header>
+            <img class="banniere" src="images/ban-accueil.jpg" alt="Article 1" width="135" height="135"/>
          <?php require File::build_path(array("view", "menu.php")); ?>
         </header>
+
             <main onclick="closeNav()">
    
 
