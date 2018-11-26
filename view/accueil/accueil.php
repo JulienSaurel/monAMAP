86a63f798e0e90193bf0eb1c29d544c1d65a22a9
Julien Saurel <julien.saurel11@gmail.com>
2018-11-26 12:31:39 +0100

    css accueil

--- view/accueil/accueil.php
+++ view/accueil/accueil.php
@@ -1,7 +1,22 @@
 <h1> Bienvenue sur le site de l'AMAP de la région Occitanie </h1>
 
 <!-- aside sur le coté gauche -->
-<aside>
+
+
+
+<!-- article pour l'essentiel de la page -->
+
+ 
+ <section class="usercontent">
+  <article>
+    <h2>Présentation de l'AMAP.</h2>
+    <img src="./images/section1-accueil.jpg" alt="AMAP partage"/> <!--width="925" height="335"-->
+    <p>
+    L'AMAP d'O regroupe plusieurs maraîchers et éveleurs de la région Occitanie, qui vous proposent leurs produits BIO. Mauris ante tellus, egestas nec arcu eget, hendrerit laoreet velit. Pellentesque id urna in massa scelerisque fermentum ac vel leo. Curabitur sem sapien, feugiat vel varius sed, condimentum vitae erat. Vestibulum non velit augue. Maecenas pretium in ex sed luctus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis vel sodales lacus. Phasellus diam massa, tincidunt a dui a, porttitor posuere nisi. Maecenas arcu risus, volutpat sit amet cursus sed, facilisis in nisi.
+    </p>
+    <a href="?action=display2nd&controller=nousConnaitre">En savoir plus...</a></p> <!--redirection vers la page AMAP-->
+  </article>
+  <aside>
   <div>
     <h4>Actualités</h4>
     <p>Prochaine rencontre à Saint-Jean-de-Védas. <br/>
@@ -21,22 +36,11 @@
     <iframe src="https://www.google.com/maps/d/embed?mid=1jXR-N1Ge3qSqQ48tiTxcWRYDfhcAoGb2" width="135" height="135"></iframe>
   </div>
 </aside>
-
-
-<!-- article pour l'essentiel de la page -->
-<article>
- 
- <section>
-    <h2>Présentation de l'AMAP.</h2>
-    <img src="./images/section1-accueil.jpg" alt="AMAP partage"/> <!--width="925" height="335"-->
-    <p>
-    L'AMAP d'O regroupe plusieurs maraîchers et éveleurs de la région Occitanie, qui vous proposent leurs produits BIO. Mauris ante tellus, egestas nec arcu eget, hendrerit laoreet velit. Pellentesque id urna in massa scelerisque fermentum ac vel leo. Curabitur sem sapien, feugiat vel varius sed, condimentum vitae erat. Vestibulum non velit augue. Maecenas pretium in ex sed luctus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis vel sodales lacus. Phasellus diam massa, tincidunt a dui a, porttitor posuere nisi. Maecenas arcu risus, volutpat sit amet cursus sed, facilisis in nisi.
-    </p>
-    <a href="?action=display2nd&controller=nousConnaitre">En savoir plus...</a></p> <!--redirection vers la page AMAP-->
-  </section>
-
-
-  <section>
+<div class="clear"></div>
+</section>
+  
+  <section class="usercontent">
+  <article>
     <h2>Q'est-ce qu'une AMAP ?</h2>
     <img src="./images/section2-accueil.jpg" alt="AMAP présentation"/> <!--width="925" height="335"-->
     <p>
@@ -47,14 +51,23 @@
     	<li>Consulter les contrats que nos partenaires proposent</li>
     	<li>Adhérer à l'AMAP et choisir son ou ses contrats</li>
     </p>
-  </section>
+  </article>
+  <aside>
+    <h1> ASIDE </h1>
+  </aside>
+</section>
 
-
-  <section>
+  <section class="usercontent">
+  <article>
     <h2>Notre engagement dans le BI0</h2>
     <img src="./images/section3-accueil.jpg" alt="AMAP biologique"/> <!--width="925" height="335"-->
     <p>Ut hendrerit ex ac libero venenatis, sit amet elementum dolor porttitor. Vivamus semper, erat non facilisis pellentesque, mauris ipsum ullamcorper lacus, ac egestas tortor turpis eu ligula. Nulla lacinia hendrerit libero, nec sollicitudin enim fermentum vitae. Nullam vestibulum, leo gravida luctus vehicula, diam dolor condimentum nulla, in pretium orci massa non erat. Duis gravida est porttitor, efficitur tellus ut, tincidunt est. In hac habitasse platea dictumst. Aliquam semper imperdiet ligula, sed vulputate dolor ultrices ut. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras feugiat maximus libero, in ultrices diam eleifend nec. Sed ut erat porttitor, vulputate dui quis, volutpat lorem.</p>
+  </article>
+  <aside>
+    <h1> ASIDE </h1>
+  </aside>
+
    </section>
 
 
-</article>
+
