b2eb1eab30427de6ab07324b02b9f12413b32f17
Louise R <louiserobert2510@gmail.com>
2018-11-29 14:22:41 +0100

    rectifications mineures

--- controller/ControllerAdherent.php
+++ controller/ControllerAdherent.php
@@ -119,8 +119,7 @@
 			{
 				$login = $_POST['idAdherent'];
 				$pw = Security::chiffrer($_POST['pw']);
-				var_dump($_POST);
-				var_dump($_SESSION);
+				
 				if (ModelAdherent::select($_POST['idAdherent']))
 				{
 					if (ModelAdherent::select($login)->checkPW($login, $pw))
--- view/nousConnaitre/connaitre.php
+++ view/nousConnaitre/connaitre.php
@@ -3,7 +3,7 @@
 		<h1>Nous connaitre</h1>
 		
 		<h4>L'AMAP</h4>
-		<div id="description"><p>description de l'AMAP</p></div>
+		<div id="description"><p>L'AMAP d'O regroupe plusieurs maraîchers et éveleurs de la région Occitanie, qui vous proposent leurs produits BIO.</p></div>
 		
 		<h4><a href="?action=display4th&controller=nousConnaitre"> Nous trouver </a></h4>
 		<div id="localisation"><p> Lorem Ipsum </p><div>
