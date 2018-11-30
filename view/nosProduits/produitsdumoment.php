5b043a9fd3232657502230660639e15993f0d3ce
Senhaji Salma <salma.senhaji@etu.umontpellier.fr>
2018-11-30 11:01:33 +0100

    modif nos produits du moment

--- view/nosProduits/produitsdumoment.php
+++ view/nosProduits/produitsdumoment.php
@@ -1,8 +1,35 @@
+<link rel="stylesheet" href="css/styleproduits.css" />
 <h1> Produits du moment: </h1>
 
+<ul>
+  <li><a href="#legumes"> Légumes </a></li>
+  <li><a href="#fruits"> Fruits </a></li>
+  <li><a href="#saison">Produits de saison </a></li>
+</ul>
+
 <div class="legumes">
-         <img src="images/carottes.jpg" alt="carottes" />
-         <img src="images/courgettes.jpg" alt="courgettes" />
-         <img src="images/artichaud.jpeg" alt="artichaud" />
-          
+	<div>
+		 <h2 id="legumes">Légumes</h2>
+         <img src="images/carotte.jpg"/>
+         <img src="images/courgette.jpeg"  />
+    </div>
+
+    <div>   
+         <h2 id="fruits">Fruits</h2>
+         <img src="images/fraise.jpg" />
+         <img src="images/cerise.jpg" />
+         <img src="images/framboise.jpg" />
+    </div>
+
+    <div>
+    	<h2 id="saison">Produits de saison</h2>
+    	<img src="images/marron.jpg" />
+
 </div>
+
+<!--*img {
+    width: 300px;
+    height: 300px;
+}-->
+
+
