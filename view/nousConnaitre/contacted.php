bad19654c75129510338709e6a64c0b57d122732
Louise R <louiserobert2510@gmail.com>
2018-11-30 08:22:07 +0100

    view contacted améliorée

--- view/nousConnaitre/contacted.php
+++ view/nousConnaitre/contacted.php
@@ -1,1 +1,5 @@
 <h1> Merci ! </h1>
+
+<p> Les administrateurs de l'AMAP d'O ont bien reçu votre message, ils s'efforcent de le traiter au plus vite.</p>
+
+<p> En attendant, pourquoi ne pas <a href="?action=display1st&controller=laVieAlAMAP" > lire les derniers articles </a> ? </p>
