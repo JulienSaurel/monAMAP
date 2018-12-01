643bb8367bf846fb7b27ed9fa1a405dd22fe8f62
Leila MABROUK <mabroukl@icloud.com>
2018-12-01 16:16:08 +0100

    nos producteurs sont dynamisés hehe (version 1.0)

--- controller/ControllerNosProduits.php
+++ controller/ControllerNosProduits.php
@@ -1,7 +1,19 @@
 <?php 
+
 class ControllerNosProduits
 {
     protected static $object='nosProduits';
+
+    public static function readAllProd(){
+        $sql = "SELECT * FROM Adherent A WHERE A.estProducteur=:prod";
+        $req_prep = Model::$pdo->prepare($sql);
+        $values = array(
+            "prod" => '1');
+        $req_prep->execute($values);
+        $req_prep->setFetchMode(PDO::FETCH_CLASS, 'ModelAdherent');
+        return $req_prep->fetchAll();
+        
+    }
 
 	public static function display()
 	{
@@ -13,10 +25,11 @@ class ControllerNosProduits
 
     public static function display1st()
     {
+        $tab_prod = self::readAllProd();
         $controller ='nosProduits';
         $view = 'nosproducteurs';
         $pagetitle = 'Nos Producteurs';
-        require File::build_path(array('view','view.php')); 
+        require File::build_path(array('view','view.php'));    
     }
 
     public static function display2nd()
@@ -34,4 +47,7 @@ class ControllerNosProduits
     $pagetitle = 'Error 404';
     require File::build_path(array('view','view.php'));
     }
-} ?>
+
+
+} 
+?>
