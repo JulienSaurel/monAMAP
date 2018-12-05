45c784590447188722d60049de1d64c42fcdca41
Leila MABROUK <mabroukl@icloud.com>
2018-12-05 11:26:05 +0100

    class prod

--- view/nosProduits/nosproducteurs.php
+++ view/nosProduits/nosproducteurs.php
@@ -9,7 +9,7 @@ if (!$tab_prod){
 	echo 'Pas de producteurs pour le moment';
 }else{
 	foreach ($tab_prod as $prod){
-		echo '<div><img src= "'.$prod->get(photo) . '" alt="' . $prod->get(idAdherent). '"/><h2>'. $prod->get('idPersonne->get(nomPersonne)') . '</h2><h5>' . $prod->get(ville) . '</h5><p>' . $prod->get(description) . '</p></div>';
+		echo '<div><img src= "'.$prod->get("photo") . '" alt="' . $prod->get("idAdherent"). ' class="imgprofil""/><h2>'. $prod->get('idPersonne->get(nomPersonne)') . '</h2><h5>' . $prod->get("ville") . '</h5><p>' . $prod->get("description") . '</p></div>';
 	}
 }
 
