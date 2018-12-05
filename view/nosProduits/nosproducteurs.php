e2f71204d645dc3c91fde72b32623aed6180e74d
Leila MABROUK <mabroukl@icloud.com>
2018-12-05 11:27:57 +0100

    class imgprofil

--- view/nosProduits/nosproducteurs.php
+++ view/nosProduits/nosproducteurs.php
@@ -10,7 +10,7 @@ if (!$tab_prod){
 }else{
 	foreach ($tab_prod as $prod){
 
-		echo '<div><img src= "'.$prod->get('photo') . '" alt="' . $prod->get('idAdherent'). '"/><h2>'. $prod->get('idPersonne->get(nomPersonne)') . '</h2><h5>' . $prod->get('ville') . '</h5><p>' . $prod->get('description') . '</p></div>';
+		echo '<div><img src= "'.$prod->get('photo') . '" alt="' . $prod->get('idAdherent'). ' class="imgprofil" "/><h2>'. $prod->get('idPersonne->get(nomPersonne)') . '</h2><h5>' . $prod->get('ville') . '</h5><p>' . $prod->get('description') . '</p></div>';
 
 	}
 }
