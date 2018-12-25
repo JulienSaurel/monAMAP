ec469774a2e32e15c48d3f3f676497aa9f41bc9d
Leila MABROUK <mabroukl@icloud.com>
2018-12-25 13:07:32 +0100

    fix ajout d'article et com 1.2

--- controller/ControllerMonProfil.php
+++ controller/ControllerMonProfil.php
@@ -9,167 +9,157 @@ class ControllerMonProfil
 
     protected static $object='monProfil';
 
+//affiche la page "voir mon profil"
     public static function profile()
     {   
+      //si l'utilisateur est connecté 
         if (isset($_SESSION['login'])) {
+          //on récupère les informations de l'adhérent
             $a = ModelAdherent::select($_SESSION['login']);
+            //on récupère les informations dans la table "Personne" lié l'adhérent
             $p = ModelPersonne::select($a->get('mailPersonne'));
         $controller ='monProfil';
         $view = 'voirmonprofil';
         $pagetitle = 'Mon Profil';
         require File::build_path(array('view','view.php'));
-        } else {
+        } 
+      //sinon erreur
+        else {
             self::error();
         }
     }
 
-
-//Modifications adresses Mail
-    // public static function updateAdrM(){
-    //   if (isset($_SESSION['login'])) {
-
-    //   $view = 'updateAdrM';
-    //   $pagetitle = 'Modifier l\'adresse mail';
-    //   require File::build_path(array('view','view.php'));
-    // }
-    // else {
-    //   self::error();
-    // }
-    // }
-
-    // public static function updatedAdrM(){
-    //     if (isset($_SESSION['login'])) {
-    //        //update dans la table Personne
-    //         $a=$_POST['newadrM'];
-    //         $primaryP='mailPersonne';
-    //         $table_nameP='Personne';
-    //         $b=ModelAdherent::select($_SESSION['login']);
-    //         $primary_valueP=$b->get('mailPersonne');
-    //         Model::update($primaryP, $primary_valueP, $table_nameP, array("mailPersonne"=>$a));
-
-    //        //update dans la table Adherent
-    //         $primaryA='idAdherent';
-    //         $table_nameA='Adherent';
-    //         $primary_valueA=$_SESSION['login'];
-    //         Model::update($primaryA, $primary_valueA, $table_nameA, array("mailPersonne"=>$a));
-
-
-    //         //redirection
-    //         self::profile();
-    //     } else {
-    //         self::error();
-    //     }
-    // }
-
-//Modifications Adresse Postale OK
+//redirige vers la page de modifications Adresse Postale
     public static function updateAdrP(){
+      //si l'utilisateur est connecté 
       if (isset($_SESSION['login'])) {
         $view = 'updateAdrP';
         $pagetitle = 'Modifier l\'adresse postale';
         require File::build_path(array('view','view.php'));
       }
+      //sinon erreur
       else {
         self::error();
       }
     }
 
+//action de modification Adresse Postale
     public static function updatedAdrP(){
+      //si l'utilisateur est connecté 
       if (isset($_SESSION['login'])) {
 
       $a=$_POST['newadrL'];
       $b=$_POST['newVille'];
       ModelAdherent::update(array("adressePostaleAdherent"=>$a, "ville"=>$b, "idAdherent"=>$_SESSION['login']));
       self::profile();
-    }
-    else {
-      self::error();
-    }
+      }
+      //sinon erreur
+      else {
+        self::error();
+      }
     }
 
-//Modifications PW ok
+//redirige vers la page de modifications PW
     public static function updatePW(){
-        if (isset($_SESSION['login'])) {
-            $view = 'updatePW';
-            $pagetitle = 'Modifier le mot de passe';
-            require File::build_path(array('view','view.php'));
-        } else {
-          self::error();
-        }
+      //si l'utilisateur est connecté 
+      if (isset($_SESSION['login'])) {
+          $view = 'updatePW';
+          $pagetitle = 'Modifier le mot de passe';
+          require File::build_path(array('view','view.php'));
+        } 
+      //sinon erreur
+      else {
+        self::error();
+      }
     }
 
+//action de modification PW
     public static function updatedPW(){
-        if (isset($_SESSION['login'])) {
-            $a=$_POST['oldpw'];
-            $achiffre=Security::chiffrer($a);
-            $b=$_POST['newpw'];
-            $bchiffre=Security::chiffrer($b);
-            $c=$_POST['newpw_c'];
-            $mail=$_SESSION['login'];
-            $mdp=ModelAdherent::select($_SESSION['login']);
-            $mdpv = $mdp->get('PW_Adherent');
-            //var_dump(ModelUtilisateur::getPwByMail($_SESSION['login']));
-            //var_dump($mdpv);
+      //si l'utilisateur est connecté 
+      if (isset($_SESSION['login'])) {
+          $a=$_POST['oldpw'];
+          $achiffre=Security::chiffrer($a);
+          $b=$_POST['newpw'];
+          $bchiffre=Security::chiffrer($b);
+          $c=$_POST['newpw_c'];
+          $mail=$_SESSION['login'];
+          $mdp=ModelAdherent::select($_SESSION['login']);
+          $mdpv = $mdp->get('PW_Adherent');
+          //var_dump(ModelUtilisateur::getPwByMail($_SESSION['login']));
+          //var_dump($mdpv);
 
-            if ($achiffre== $mdpv){
-                if($b==$c){
-                  ModelAdherent::update(array("PW_Adherent"=>$bchiffre, "idAdherent"=>$_SESSION['login']));
-                  self::profile();
-                } 
-                else {
-                  echo 'les deux nouveaux mots ne correspondent pas !';
-                  $view = 'updatePW';
-                  $pagetitle = 'Erreur correspondance';
-                  require File::build_path(array('view','view.php'));
-                }
-            }
-            else {
-                echo 'L\'ancien mot de passe est faux';
+          if ($achiffre== $mdpv){
+              if($b==$c){
+                ModelAdherent::update(array("PW_Adherent"=>$bchiffre, "idAdherent"=>$_SESSION['login']));
+                self::profile();
+              } 
+              else {
+                echo 'les deux nouveaux mots ne correspondent pas !';
                 $view = 'updatePW';
-                $pagetitle = 'Erreur dans l\'ancien mot de passe';
+                $pagetitle = 'Erreur correspondance';
                 require File::build_path(array('view','view.php'));
-            }
-        } 
-        else {
-            self::error();
-        }
+              }
+          }
+          else {
+              echo 'L\'ancien mot de passe est faux';
+              $view = 'updatePW';
+              $pagetitle = 'Erreur dans l\'ancien mot de passe';
+              require File::build_path(array('view','view.php'));
+          }
+      } 
+      //sinon erreur
+      else {
+          self::error();
+      }
   }
 
-//Modifications description ok
-
+//redirige vers la page de modifications Description
   public static function updateDes(){
     $view = 'updateDes';
     $pagetitle = 'Description';
     require File::build_path(array('view','view.php')); 
   }
 
+//action de modification Description
   public static function updatedDes(){
+    //si l'utilisateur est connecté 
     if (isset($_SESSION['login'])) {
       $a=$_POST['newdesc'];
       ModelAdherent::update(array("description"=>$a, "idAdherent"=>$_SESSION['login']));
       self::profile();
     }
+    //sinon erreur    
     else {
       self::error();
     }
   }
 
-  //Modifications photos ok
-
+//redirige vers la page de modifications Photo
 public static function updatePhoto(){
-    $view = 'updatePhoto';
-    $pagetitle = 'Photo';
-    require File::build_path(array('view','view.php')); 
-}
-
-public static function updatedPhoto(){
+    //si l'utilisateur est connecté 
     if (isset($_SESSION['login'])) {
-      $a=$_POST['photo'];
-      ModelAdherent::update(array("photo"=>$a, "idAdherent"=>$_SESSION['login']));
-      self::profile();
+      $view = 'updatePhoto';
+      $pagetitle = 'Photo';
+      require File::build_path(array('view','view.php'));
     }
+    //sinon erreur
     else {
       self::error();
-    }
+    } 
+}
+
+//action de modification Photo
+public static function updatedPhoto(){
+      //si l'utilisateur est connecté 
+      if (isset($_SESSION['login'])) {
+        $a=$_POST['photo'];
+        ModelAdherent::update(array("photo"=>$a, "idAdherent"=>$_SESSION['login']));
+        self::profile();
+      }
+      //sinon erreur
+      else {
+        self::error();
+      }
 }
 
     public static function display2nd()
