82471355b69f78835b08d6c58becf72f0f491a1a
LouiseRobert <43638358+LouiseRobert@users.noreply.github.com>
2018-12-28 16:27:21 +0100

    contenu

--- view/nosContrats/legumes.php
+++ view/nosContrats/legumes.php
@@ -1,5 +1,5 @@
 <h1> Nos contrats végétaux </h1>
 
-<p> Cette semaine le panier végétal se compose de blablabla... </p>
+<p> Cette semaine le panier végétal se compose de Sed feugiat velit sit amet dui pellentesque, ac sodales diam blandit. Morbi sed posuere lorem. Phasellus imperdiet semper pulvinar. Phasellus libero nibh, semper sit amet mi aliquet, maximus viverra mi. Aliquam vitae malesuada nisl, nec finibus diam. Cras euismod porta ipsum, et accumsan libero venenatis non. Nullam magna tortor, tincidunt consequat enim id, euismod lobortis metus. Fusce nunc justo, porta nec aliquam rhoncus, porttitor sed nisi. Nullam justo mauris, feugiat a euismod vitae, posuere id metus.  </p>
 
 <p> Vous souhaitez souscire à ce contrat ? </p> <a href="?action=display5th&controller=nosContrats" > cliquez ici </a>
